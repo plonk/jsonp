8c17ec2eba6a3f7c33290018edf876249b2cf4c0
Yoteichi <plonk@piano.email.ne.jp>
2018-05-07 04:13:49 +0900

    more コマンド

---
+++ more.scm
@@ -0,0 +1,33 @@
+(define (iter lines index)
+  (cond ((null? lines) 'done)
+        ((< index 23)
+         (print (car lines))
+         (iter (cdr lines) (+ 1 index)))
+        (else
+         (display (string-append "\x1b[7m" "--More--" "\x1b[0m"))
+         (let ((ch (read-char)))
+           ;; erase entire line and jump to column 0
+           (display "\x1b[2K\r")
+           (cond ((eq? ch "q") 'done)
+                 ((eq? ch " ")
+                  (iter lines 0))
+                 (else
+                  (iter lines (- index 1))))))))
+
+(define (but-last ls)
+  (reverse (cdr (reverse ls))))
+
+(define (main)
+  (if (null? *argv*)
+      (begin
+        (print "Usage: more FILENAME")
+        (exit)))
+  (let ((buffer (sys-get-file-contents (car *argv*))))
+    (if (eq? "" buffer)
+        (exit)
+        (let ((lines (split "\\n" buffer)))
+          (if (eq? "\n" (string-ref buffer (- (string-length buffer) 1)))
+              (set! lines (but-last lines)))
+          (iter lines 0)))))
+
+(main)
